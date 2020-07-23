ddd64cd2b881639cd9e810e06df953d11f08eecd
foxten <tenaysiafox@gmail.com>
2020-07-23 11:15:57 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,33 @@
 class ArtistsController < ApplicationController
+    before_action :find_artist, only: [:show]
+   
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+        if @artist.save
+            redirect_to @artist
+        else
+            render :new
+        end
+    end
+
+
+    private
+    def find_artist
+        @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+        params.require(:artist).permit(:name, :age, :title)
+    end
 end
